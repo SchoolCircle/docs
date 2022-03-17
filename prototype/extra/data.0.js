d7e709bf7ad67ca0fb7715b91d7f47665beed24d
Hardline <gnn1281015959@163.com>
2022-03-17 22:26:17 +0800

    update the prototype

--- prototype/extra/data.0.js
+++ prototype/extra/data.0.js
@@ -1,1 +1,1 @@
-window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"gD5wcilFr8gulqO2CqCUl","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"l0v096numdeot1","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
